545db26e29b9b5247fb64204d4acd2cdea87d7e9
Florian Koudjonou <floriankoud@hotmail.com>
2023-03-22 13:46:39 -0400

    ADASCOPE-79 trying to fix the formatting

--- uart_testing/src/uart_testing.adb
+++ uart_testing/src/uart_testing.adb
@@ -12,7 +12,8 @@ procedure Uart_Testing is
     --Data : GNAT.Serial_Communications.Buffer_Type(1..100);
     Data : Ada.Streams.Stream_Element_Array (1 .. 10);
 
-    Test : Stream_Element_Offset;
+    Test : Stream_Element_Offset := 0;
+    Char : Character;
 begin
     -- Open the serial port
     GNAT.Serial_Communications.Open(Port, "/dev/ttyACM0");
@@ -20,6 +21,20 @@ begin
     -- Read data from the porttype
     loop
         GNAT.Serial_Communications.Read(Port, Data, Test);
+        for I in Data'Range loop
+            if (Character'Val (Data(I))) = ASCII.CR then
+                Put_Line ("");
+            elsif (Character'Val (Data(I))) = ASCII.LF then
+                Put_Line ("");
+            else
+                Put(Character'Image (Character'Val (Data(I))));
+            end if;
+        end loop;
+
+        --for I in Data'Range loop
+        --    Put_Line (Character'Image (Data(I)));
+        --end loop;
+
     end loop;
 
     -- Close the port
