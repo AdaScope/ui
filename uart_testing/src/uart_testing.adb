0e2171361bc7ddbc7a4f1f646f15ca46d476df5a
Florian Koudjonou <floriankoud@hotmail.com>
2023-03-22 16:50:39 -0400

    ADASCOPE-79 formatting working. Thanks Bing chat

--- uart_testing/src/uart_testing.adb
+++ uart_testing/src/uart_testing.adb
@@ -1,40 +1,28 @@
-with Ada.Text_IO;                   use Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 with GNAT.Serial_Communications;
-with Ada.Streams;                   use Ada.Streams;
-
-with Ada.Numerics.Elementary_Functions;
+with Ada.Streams; use Ada.Streams;
 
 procedure Uart_Testing is
-
-    -- For UART
     Port : GNAT.Serial_Communications.Serial_Port;
-    --Data : GNAT.Serial_Communications.Stream_Element_Array(1..100);
-    --Data : GNAT.Serial_Communications.Buffer_Type(1..100);
-    Data : Ada.Streams.Stream_Element_Array (1 .. 10);
+    Data : Stream_Element_Array (1 .. 1);
+    Line : String (1 .. 100);
+    Line_Index : Natural := Line'First;
+    Offset : Stream_Element_Offset := 1;
 
-    Test : Stream_Element_Offset := 0;
-    Char : Character;
 begin
     -- Open the serial port
     GNAT.Serial_Communications.Open(Port, "/dev/ttyACM0");
 
-    -- Read data from the porttype
+    -- Read data from the port
     loop
-        GNAT.Serial_Communications.Read(Port, Data, Test);
-        for I in Data'Range loop
-            if (Character'Val (Data(I))) = ASCII.CR then
-                Put_Line ("");
-            elsif (Character'Val (Data(I))) = ASCII.LF then
-                Put_Line ("");
-            else
-                Put(Character'Image (Character'Val (Data(I))));
-            end if;
-        end loop;
-
-        --for I in Data'Range loop
-        --    Put_Line (Character'Image (Data(I)));
-        --end loop;
-
+        GNAT.Serial_Communications.Read(Port, Data, Offset);
+        if Character'Val(Data(1)) = ASCII.LF then
+            Put_Line(Line(Line'First .. Line_Index - 1));
+            Line_Index := Line'First;
+        else
+            Line(Line_Index) := Character'Val(Data(1));
+            Line_Index := Line_Index + 1;
+        end if;
     end loop;
 
     -- Close the port
