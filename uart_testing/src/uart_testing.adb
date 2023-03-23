94b2a6880a02481378755061e66b3063f552ec0a
Florian Koudjonou <floriankoud@hotmail.com>
2023-03-23 09:13:09 -0400

    ADASCOPE-79 added more comments

--- uart_testing/src/uart_testing.adb
+++ uart_testing/src/uart_testing.adb
@@ -4,23 +4,25 @@ with Ada.Streams; use Ada.Streams;
 
 procedure Uart_Testing is
     Port : GNAT.Serial_Communications.Serial_Port;
-    Data : Stream_Element_Array (1 .. 1);
-    Line : String (1 .. 100);
+    Buffer : Stream_Element_Array (1 .. 1);
+    Line : String (1 .. 10);
     Line_Index : Natural := Line'First;
     Offset : Stream_Element_Offset := 1;
-
 begin
     -- Open the serial port
     GNAT.Serial_Communications.Open(Port, "/dev/ttyACM0");
 
-    -- Read data from the port
     loop
-        GNAT.Serial_Communications.Read(Port, Data, Offset);
-        if Character'Val(Data(1)) = ASCII.LF then
-            Put_Line(Line(Line'First .. Line_Index - 1));
+        -- Read data from the port
+        GNAT.Serial_Communications.Read(Port, Buffer, Offset);
+
+        if Character'Val(Buffer(1)) = ASCII.LF then
+            -- We write our entire line to the terminal
+            Put_Line(Line(Line'First .. Line_Index- 1));
             Line_Index := Line'First;
         else
-            Line(Line_Index) := Character'Val(Data(1));
+            -- We write the current character to the current index of our line
+            Line(Line_Index) := Character'Val(Buffer(1));
             Line_Index := Line_Index + 1;
         end if;
     end loop;
