98c326c0716789beb45cf2ce2ac6d0a228d97241
Badass <bkaan006@uottawa.ca>
2023-12-01 16:33:37 -0500

    modified test files

--- user_interface/tests/run_tests.adb
+++ user_interface/tests/run_tests.adb
@@ -1,7 +1,11 @@
-with AUnit.Runner;
-with Uart_Unit_Tests;
+with AUnit.Run;
+with Math_Suite; use Math_Suite;
+with Aunit.Reporter.Text;
 
 procedure Run_Tests is
+   procedure Runner is new AUnit.Run.Test_Runner (Suite);
+   Reporter : Aunit.Reporter.Text.Text_Reporter;
 begin
-   AUnit.Runner.Run_Test_Suite ("Uart_Unit_Tests");
+   Runner (Reporter);
+   
 end Run_Tests;
--- user_interface/tests/uart_test.adb
+++ user_interface/tests/uart_test.adb
@@ -1,6 +1,6 @@
 with AUnit.Assertions;
+with Uart;
 
-package Uart_Unit_Tests is
 
    procedure Test_Read_Function is
       Num_Samples : Integer := 5;
@@ -9,23 +9,23 @@ package Uart_Unit_Tests is
       Result := Uart.Read(Num_Samples);
 
       -- Assert that the result array has the expected length
-      AUnit.Assertions.Assert (Result'Length = Num_Samples, "Unexpected array length");
+      AUnit.Assertions.Assert(Result'Length = Num_Samples, "Unexpected array length");
 
       -- Loop through the result array to check its values
       for I in Result'Range loop
          case I is
             when 1 =>
                -- Check the first value in the result array
-               AUnit.Assertions.Assert (Result(I) = 42.0, "Unexpected value in the array at index 1");
+               AUnit.Assertions.Assert(Result(I) = 42.0, "Unexpected value in the array at index 1");
             when 2 =>
                -- Check the second value in the result array
-               AUnit.Assertions.Assert (Result(I) = 3.14, "Unexpected value in the array at index 2");
+               AUnit.Assertions.Assert(Result(I) = 3.14, "Unexpected value in the array at index 2");
             -- Add more cases as needed to check other values in the array
             when others =>
                -- Handle unexpected indices (optional)
-               AUnit.Assertions.Assert (False, "Unexpected index in the result array");
+               AUnit.Assertions.Assert(False, "Unexpected index in the result array");
          end case;
       end loop;
    end Test_Read_Function;
 
-end Uart_Unit_Tests;
+
